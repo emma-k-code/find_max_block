543fbbd4eb0af2a201267e5e2402c4d24231460b
Emma <k54979802@gmail.com>
2016-08-01 06:44:27 +0000

    更改部分程式流程

--- findMaxBlock.php
+++ findMaxBlock.php
@@ -1,4 +1,5 @@
 <?php
+$time = microtime();
 $origin = array(
         array(1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
         array(1, 1, 0, 1, 1, 0, 0, 0, 0, 0),
@@ -17,10 +18,9 @@ $origin = array(
     
     foreach ($check as $y=>$column) {
         foreach ($column as $x=>$n) {
-            $haveOne = array(); // 儲存相鄰的1的座標
+            $haveOne = array(); // 儲存為1的座標
             $needFind = array();
             if ($n==1) {
-                $check[$y][$x] = "0"; // 將找過的都改為0
                 $haveOne[] = array($y,$x);
                 find($y,$x); // 搜尋相鄰區域
             }
@@ -59,36 +59,32 @@ $origin = array(
     
     function find($y,$x) {
         global $check;
-        global $haveOne;
         global $needFind;
-        global $i;
+        global $haveOne;
+        
+        $check[$y][$x] = 0 ; // 將找過的改為0
+        
         // 加入要搜尋的右下左上的座標
-        if ($check[$y][$x+1] == 1) {
-            $needFind[] = array($y,$x+1); // 右
-        }
-        if ($check[$y+1][$x] == 1) {
-            $needFind[] = array($y+1,$x); // 下
-        }
-        if ($check[$y][$x-1] == 1) {
-            $needFind[] = array($y,$x-1); // 左
-        }
-        if ($check[$y-1][$x] == 1) {
-            $needFind[] = array($y-1,$x); // 上
-        }
+        $needFind[] = array($y,$x+1); // 右
+        $needFind[] = array($y+1,$x); // 下
+        $needFind[] = array($y,$x-1); // 左
+        $needFind[] = array($y-1,$x); // 上
         
         // 搜尋$needFind中的座標
         foreach ($needFind as $key=>$value) {
             $findY = $value[0];
             $findX = $value[1];
             
+            unset($needFind[$key]); // 將找過的座標刪除
             if ($check[$findY][$findX]==1) {
                 $haveOne[] = array($findY,$findX);
+                find($findY,$findX); // 搜尋該座標的相鄰區域
             }
-            $check[$findY][$findX] = "0";
-            unset($needFind[$key]); // 將找過的座標刪除
-            find($findY,$findX); // 搜尋該座標的相鄰區域
+            
         }
         
     }
+    
+    echo microtime() - $time;
     
 ?>
